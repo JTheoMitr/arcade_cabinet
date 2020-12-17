60ae4122f2b00931c1bd9b0ffb8ddea98dd8ca6a
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-12-17 16:42:46 -0600

    created company model and seed file

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 
 ## Set up Company Model
-## 
+- table
+- model
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Company.create(name: "Nintendo", location: "Kyoto, Japan", age: 131)
+Company.create(name: "Bethesda", location: "Rockville, Maryland", age: 19)
+Company.create(name: "Capcom", location: "Osaka, Japan", age: 41)
+Company.create(name: "CD Projekt", location: "Warsaw, Poland", age: 26)
