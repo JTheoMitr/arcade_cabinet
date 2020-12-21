ce2a09512e01de18831c8c30442a9c150de041ff
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-12-21 00:49:51 -0600

    added edit form and update action companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,9 +1,5 @@
 class CompaniesController < ApplicationController
 
-    get "/companies" do
-        @companies = Company.all.sort_by { |company| company.name }
-        erb :"companies/index"
-      end
 
       get "/companies/new" do
         erb :"companies/new"
@@ -16,9 +12,32 @@ class CompaniesController < ApplicationController
         redirect to "/companies/#{@company.id}"
       end
 
+      get "/companies" do
+        @companies = Company.all.sort_by { |company| company.name }
+        erb :"companies/index"
+      end
+
       get "/companies/:id" do
         @company = Company.find_by(id: params[:id])
+        if @company
         erb :"companies/show"
+        else
+          redirect "/companies"
+        end
     end
+
+    get '/companies/:id/edit' do
+      @company = Company.find_by(id: params[:id])
+      erb :"companies/edit"
+    end
+
+    patch '/companies/:id' do
+      @company = Company.find_by(id: params[:id])
+      attrs = params[:company]
+      @company.update(attrs)
+      redirect to "/companies/#{@company.id}"
+    end
+
+   
 
 end
